85a52e1a697899a2ad609b548a380d9e77384c26
Helal-AIUB <helalaiub8009@gmail.com>
2026-07-30 15:32:43 +0600

    apiClient.ts file updated cause render backend linked

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/store/'
-  // baseURL: 'https://zenmart-backend.onrender.com/store/',
+  // baseURL: 'http://127.0.0.1:8000/store/'
+  baseURL: "https://zenmart-backend.onrender.com/store/",
 });
 
 // import axios from 'axios';
