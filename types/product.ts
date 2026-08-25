7559e1234d509fb81e803ed84fa29609c8b9df28
Helal-AIUB <helalaiub8009@gmail.com>
2026-08-25 14:32:02 +0600

    fix: add images property to Product type

--- types/product.ts
+++ types/product.ts
@@ -1,3 +1,18 @@
+// export interface Product {
+//   id: number;
+//   title: string;
+//   slug: string;
+//   description: string | null;
+//   unit_price: string | number;
+//   inventory: number;
+//   collection: number;
+// }
+
+export interface ProductImage {
+  id: number;
+  image: string;
+}
+
 export interface Product {
   id: number;
   title: string;
@@ -6,4 +21,7 @@ export interface Product {
   unit_price: string | number;
   inventory: number;
   collection: number;
+
+  // Multiple product images (optional)
+  images?: ProductImage[];
 }
