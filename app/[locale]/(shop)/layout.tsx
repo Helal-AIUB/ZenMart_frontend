594be23e4a05f97c68acec9fdb5fc1e77056aa56
Helal-AIUB <helalaiub8009@gmail.com>
2026-09-15 11:42:58 +0600

    what'sapp integrated - Now user can communicate, message anytime using what'sapp

--- app/[locale]/(shop)/layout.tsx
+++ app/[locale]/(shop)/layout.tsx
@@ -2,10 +2,11 @@ import ReactQueryProvider from '@/providers/ReactQueryProvider';
 import NavbarWrapper from '@/components/NavbarWrapper';
 import Footer from '@/components/Footer';
 import CartDrawer from "@/components/CartDrawer";
-import '@/app/globals.css'; // 🟢 Fix: Absolute path to prevent breakages
+import '@/app/globals.css'; 
 import { Toaster } from 'react-hot-toast';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
+import WhatsAppWidget from "@/components/WhatsAppWidget";
 
 export default async function ShopLayout({ children }: { children: React.ReactNode }) {
   const messages = await getMessages();
@@ -18,6 +19,7 @@ export default async function ShopLayout({ children }: { children: React.ReactNo
         <CartDrawer />
         {children}
         <Footer />
+        <WhatsAppWidget />
       </ReactQueryProvider>
     </NextIntlClientProvider>
   );
